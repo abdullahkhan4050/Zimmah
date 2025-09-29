6d39af9a57c1f041c8bd8407406876751b8025c3
Abdullah khan <abdullahkh4050@gmail.com>
2025-09-29 09:09:29 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,12 +1,11 @@
-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "AIzaSyDTadMjr9cGGxyK-9T6tJXR50sl4I8b2kw",
-  authDomain: "zimmah-9b98f.firebaseapp.com",
-  projectId: "zimmah-9b98f",
-  storageBucket: "zimmah-9b98f.appspot.com",
-  messagingSenderId: "749551053297",
-  appId: "1:749551053297:web:34f6b6b823cd8cc369bc50",
-  measurementId: "G-4D7G02SYHG"
+  projectId: "studio-9616321524-14adc",
+  appId: "1:77363033055:web:173210a3cb553a0ee94a6a",
+  apiKey: "AIzaSyC4vb0lt8qaaFyOjgTMAXGFzFBxz57yNvU",
+  authDomain: "studio-9616321524-14adc.firebaseapp.com",
+  measurementId: "",
+  messagingSenderId: "77363033055",
+  storageBucket: "studio-9616321524-14adc.appspot.com",
 };
