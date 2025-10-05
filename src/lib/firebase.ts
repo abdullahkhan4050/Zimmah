189b7e955d41ed3f66adf68c02b6580d37e2900e
Abdullah khan <abdullahkh4050@gmail.com>
2025-10-05 13:23:34 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/lib/firebase.ts
+++
@@ -1,2 +0,0 @@
-// This file is being removed as it is being replaced by the new Firebase module structure.
-// The new entry point for Firebase config and initialization is now src/firebase/index.ts.
