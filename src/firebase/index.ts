641fd549debc18215d4ad95d1174cafb851fede6
Abdullah khan <abdullahkh4050@gmail.com>
2025-10-12 12:30:20 +0000

    when any image is updated in user profile, it will save into firebase st

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,3 +1,4 @@
+
 'use client';
 
 import { firebaseConfig } from '@/firebase/config';
@@ -47,7 +48,10 @@ export * from './provider';
 export * from './client-provider';
 export * from './firestore/use-collection';
 export * from './firestore/use-doc';
+export * from './auth/use-user';
 export * from './non-blocking-updates';
 export * from './non-blocking-login';
 export * from './errors';
 export * from './error-emitter';
+
+    
